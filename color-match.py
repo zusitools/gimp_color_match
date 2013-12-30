92c71e2a089a6cf6029366ffc4f974a04b95c0c9
Johannes <zusitools@web.de>
2013-12-30 15:10:23 +0100

    Use gimp_curves_spline function

--- color-match.py
+++ color-match.py
@@ -4,23 +4,16 @@ from gimpfu import *
 import os
 
 def color_match(img, drawable):
-  tempname = pdb.gimp_temp_name("curve")
   fgcolor = pdb.gimp_context_get_foreground()
   bgcolor = pdb.gimp_context_get_background()
 
-  default_line = "0 0 " + ("-1 -1 " * 15) + "255 255" + os.linesep
-  red_line = "0 0 " + str(bgcolor[0]) + " " + str(fgcolor[0]) + " " + ("-1 -1 " * 14) + "255 255" + os.linesep
-  green_line = "0 0 " + str(bgcolor[1]) + " " + str(fgcolor[1]) + " " + ("-1 -1 " * 14) + "255 255" + os.linesep
-  blue_line = "0 0 " + str(bgcolor[2]) + " " + str(fgcolor[2]) + " " + ("-1 -1 " * 14) + "255 255" + os.linesep
-
-  file_contents = "# GIMP Curves File" + os.linesep + default_line + red_line + green_line + blue_line + default_line
-
-  with open(tempname, "w") as temp_file:
-    print tempname
-    temp_file.write(file_contents)
-    print(file_contents)
-
-    pdb.plug_in_wr_curves(img, drawable, tempname, run_mode = RUN_NONINTERACTIVE)
+  try:
+    pdb.gimp_image_undo_group_start(img)
+    for (index, channel) in [(0, HISTOGRAM_RED), (1, HISTOGRAM_GREEN), (2, HISTOGRAM_BLUE)]:
+      print bgcolor[index], fgcolor[index]
+      pdb.gimp_curves_spline(drawable, channel, 6, [0, 0, bgcolor[index], fgcolor[index], 255, 255])
+  finally:
+    pdb.gimp_image_undo_group_end(img)
 
 register(
   "color-match",
